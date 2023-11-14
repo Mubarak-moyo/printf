22a809218f6f76ef681bfa8fb218a320fde659cb
Mubarak <yolkadeolu@gmail.com>
2023-11-13 19:25:02 -0800

    jj

--- helpers.c
+++ helpers.c
@@ -1,15 +1,37 @@
 #include "main.h"
 
-int print_char(va_list args) {
-    char temp_char = (char)va_arg(args, int);
-    return write(1, &temp_char, 1);
+/**
+ * print_char - Function that prints a chracter.
+ * @args: va_list of arguments.
+ *
+ * Return: The number of characters printed.
+ */
+int print_char(va_list args)
+{
+	char temp_char = (char)va_arg(args, int);
+
+	return (write(1, &temp_char, 1));
 }
 
-int print_string(va_list args) {
-    char *temp_str = va_arg(args, char *);
-    return write(1, temp_str, strlen(temp_str));
+/**
+ * print_string - Function that prints a string.
+ * @args: This is the va_list of arguments.
+ *
+ * Return: The number of characters printed.
+ */
+int print_string(va_list args)
+{
+	char *temp_str = va_arg(args, char *);
+
+	return (write(1, temp_str, strlen(temp_str)));
 }
 
-int print_percent(void) {
-    return write(1, "%", 1);
+/**
+ * print_percent - Function that prints percent.
+ *
+ * Return: The number of characters printed.
+ */
+int print_percent(void)
+{
+	return (write(1, "%", 1));
 }
